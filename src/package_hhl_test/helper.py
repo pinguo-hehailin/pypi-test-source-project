d3ad7fe84a953bf7d4e178af157c1c15137e0f51
pinguo-hehailin <hehailin@camera360.com>
2024-02-22 17:17:19 +0800

    add sub packge

--- src/package_hhl_test/helper.py
+++ src/package_hhl_test/helper.py
@@ -1,7 +1,11 @@
+from sub_module.another_helper import SomeHelper
+
+
 class Helper:
     def __init__(self):
+        self.inner_helper = SomeHelper()
         pass
 
     @staticmethod
     def get_value(a: str):
-        return f'hi, {a}'
+        return f', {a}'
---
+++ src/package_hhl_test/sub_module/another_helper.py
@@ -0,0 +1,7 @@
+class SomeHelper:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def say_hi(msg: str):
+        return f'hi {msg}[by SomeHelper]'
